8a9d6090d7651c867693a5b271162d15ec3fdf9c
Brian Bleck <bbleck@unm.edu>
2017-09-09 14:53:14 -0600

    For Enemy and Player classes: added logic to locationOOB(Point objectCenter)

--- src/TraingleGame/Enemy.java
+++ src/TraingleGame/Enemy.java
@@ -65,6 +65,10 @@ public class Enemy extends Movables{
      *         true if location is out of bounds
      */
     public boolean locationOOB(Point objectCenter){
-        return false;
+        Main m = new Main();
+        if(objectCenter.x>m.WIDTH || objectCenter.x<0 || objectCenter.y>m.HEIGHT || objectCenter.y<0){
+            return true;
+        }
+        else return false;
     }
 }
